1f2e66599f3a85855b2f159eb2088b3d825f679d
mfroeh <mfroeh0@pm.me>
2025-10-26 22:42:12 +0100

    add desktop entry

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,21 @@ use std::error::Error;
 
 use abi_stable::std_types::{RString, RVec};
 use anyrun_plugin::*;
+use freedesktop_desktop_entry::{DesktopEntry, unicase::Ascii};
 use fuzzy_matcher::{FuzzyMatcher, skim::SkimMatcherV2};
 use swayipc::{Connection, Node, NodeType};
 
-#[init]
-fn init(_config_dir: RString) -> Vec<Node> {
-    collect_windows().expect("collect sway windows")
+struct State {
+    windows: Vec<(Node, Option<DesktopEntry>)>,
+    locales: Vec<String>,
 }
 
-fn collect_windows() -> Result<Vec<Node>, Box<dyn Error>> {
+#[init]
+fn init(_config_dir: RString) -> State {
+    init_state().expect("init state")
+}
+
+fn init_state() -> Result<State, Box<dyn Error>> {
     let root = Connection::new()?.get_tree()?;
     let mut windows = Vec::new();
     let mut stack = vec![root];
@@ -20,7 +26,28 @@ fn collect_windows() -> Result<Vec<Node>, Box<dyn Error>> {
         }
         stack.extend_from_slice(&top.nodes);
     }
-    Ok(windows)
+
+    let locales = freedesktop_desktop_entry::get_languages_from_env();
+    let desktop_entries = freedesktop_desktop_entry::desktop_entries(&locales);
+    let windows: Vec<_> = windows
+        .into_iter()
+        .map(|w| {
+            if let Some(app_id) = w.app_id.clone() {
+                (
+                    w,
+                    freedesktop_desktop_entry::find_app_by_id(
+                        &desktop_entries,
+                        Ascii::new(&app_id),
+                    )
+                    .cloned(),
+                )
+            } else {
+                (w, None)
+            }
+        })
+        .collect();
+
+    Ok(State { locales, windows })
 }
 
 #[info]
@@ -33,17 +60,29 @@ fn info() -> PluginInfo {
 
 // https://github.com/anyrun-org/anyrun/blob/master/plugins/applications/src/lib.rs
 #[get_matches]
-fn get_matches(input: RString, windows: &[Node]) -> RVec<Match> {
+fn get_matches(input: RString, state: &State) -> RVec<Match> {
     let words: Vec<_> = input.split_whitespace().collect();
     let matcher = SkimMatcherV2::default();
-    let mut scored_windows: Vec<_> = windows
+    let mut scored_windows: Vec<_> = state
+        .windows
         .iter()
-        .filter_map(|n| {
+        .filter_map(|(win, entry)| {
             let scores = words.iter().map(|w| {
-                let app_id = n.app_id.clone().unwrap_or("".into());
-                let name = n.name.clone().unwrap_or("".into());
-                let mut score = matcher.fuzzy_match(&app_id, w).unwrap_or_default() * 2;
-                score += matcher.fuzzy_match(&name, w).unwrap_or_default();
+                let mut application_name = win.app_id.clone().unwrap_or("".to_string());
+                if let Some(entry) = entry {
+                    if let Some(name) = entry.name(&state.locales) {
+                        application_name = name.to_string();
+                    }
+                }
+
+                let full_window_name = win.name.clone().unwrap_or("".into());
+                let mut score = matcher
+                    .fuzzy_match(&application_name, w)
+                    .unwrap_or_default()
+                    * 2;
+                score += matcher
+                    .fuzzy_match(&full_window_name, w)
+                    .unwrap_or_default();
                 if score == 0 {
                     return None;
                 }
@@ -57,21 +96,31 @@ fn get_matches(input: RString, windows: &[Node]) -> RVec<Match> {
                     return None;
                 }
             }
-            Some((n, score))
+            Some((win, entry, score))
         })
         .collect();
-    scored_windows.sort_by_key(|f| -f.1);
+    scored_windows.sort_by_key(|f| -f.2);
 
     scored_windows
         .into_iter()
-        .map(|(n, _)| {
-            let window_title = n.name.clone().unwrap_or("".into());
-            let class = n.app_id.clone().unwrap_or("".into());
+        .map(|(win, entry, _)| {
+            let mut application_name = win.app_id.clone().unwrap_or("".to_string());
+            if let Some(entry) = entry {
+                if let Some(name) = entry.name(&state.locales) {
+                    application_name = name.to_string();
+                }
+            }
+
+            let full_window_title = win.name.clone().unwrap_or("".into());
             Match {
-                id: Some(n.id as u64).into(),
-                icon: None.into(),
-                title: class.into(),
-                description: Some(window_title.into()).into(),
+                id: Some(win.id as u64).into(),
+                icon: entry
+                    .as_ref()
+                    .and_then(|e| e.icon())
+                    .map(RString::from)
+                    .into(),
+                title: application_name.into(),
+                description: Some(full_window_title.into()).into(),
                 use_pango: false,
             }
         })
